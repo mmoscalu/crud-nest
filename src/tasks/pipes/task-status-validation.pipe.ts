c51a9c01626485b7b54c657ea004273dd8c62fff
misha <mmoskalu@yukon.org.ua>
2020-12-19 21:28:27 +0200

    add task entity

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,23 @@
+import {BadRequestException, PipeTransform} from "@nestjs/common";
+import {TaskStatus} from "../task.model";
+
+export class TaskStatusValidationPipe implements PipeTransform {
+
+    readonly allowedStatus:TaskStatus[] = [
+        TaskStatus.OPEN,
+        TaskStatus.IN_PROGRESS,
+        TaskStatus.DONE
+    ]
+    transform(status: any): string {
+        status = status.toUpperCase();
+        if (!this.isStatusValid(status)) {
+            throw new BadRequestException(`${status} is not valid status`);
+        }
+        return status;
+    }
+
+    isStatusValid(status: TaskStatus) {
+        const index: number = this.allowedStatus.indexOf(status);
+        return index !== -1;
+    }
+}
